0a85d1f313c1f5de3f9c514598712ef0030e38b2
Juanjo <jjrodriguezaldavero@gmail.com>
2022-07-18 21:49:53 +0200

    Implement recipe API

--- app/core/admin.py
+++ app/core/admin.py
@@ -22,7 +22,7 @@ class UserAdmin(BaseUserAdmin):
                     'is_active',
                     'is_staff',
                     'is_superuser',
-                ),
+                )
             }
         ),
         (_('Important dates'), {'fields': ('last_login',)}),
@@ -39,9 +39,10 @@ class UserAdmin(BaseUserAdmin):
                 'is_active',
                 'is_staff',
                 'is_superuser',
-            )
+            ),
         }),
     )
 
 
 admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Recipe)
